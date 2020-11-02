c48cb5ebfb159b30f1206594e9f6e344e965c4a4
James Njoroge <jnjoroge141@gmail.com>
2020-11-02 12:50:10 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import './randomwordpair.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
